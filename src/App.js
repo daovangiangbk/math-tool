e3c996b17da7ba2bea46119c84829c51281243df
DAOVANGIANGBK <daovangiangbk@gmail.com>
2022-11-05 21:58:53 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import MathType from "@wiris/mathtype-ckeditor5";
+
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "ckeditor5-build-classic-mathtype";
+import { useState } from "react";
+import "polyfill-math";
+import MathJax from "react-mathjax-preview";
 
 function App() {
+  const [ckData, setCkData] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Using CKEditor 5 build in React</h2>
+      <CKEditor
+        editor={ClassicEditor}
+        data={ckData}
+        onReady={editor => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+
+          setCkData(data);
+          console.log({ data });
+        }}
+        onBlur={(event, editor) => {
+          console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log("Focus.", editor);
+        }}
+        config={
+          {
+            // toolbar: ["MathType"],
+          }
+        }
+      />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <MathJax math={ckData}></MathJax>
     </div>
   );
 }
