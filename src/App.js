78839022935d7ee875dd06f6febfa8caef6516b3
DAOVANGIANGBK <daovangiangbk@gmail.com>
2023-01-09 17:57:25 +0700

    feat: tex mml to hmtl

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
+/* eslint-disable no-undef */
 import "./App.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "ckeditor5-build-classic-mathtype";
 import { useState } from "react";
+import { useEffect } from "react";
+
+async function httpGet(url) {
+  return await fetch(url).then(async res => await res.text());
+}
+
+function parseXml(text) {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(text, "text/xml");
+  console.log("xml", {
+    xx: xmlDoc.getElementsByTagName("Worksheet")[3].childNodes[2],
+  });
+  return xmlDoc.getElementsByTagName("Worksheet")[3].childNodes[2];
+  // xmlDoc.getElementsByTagName("Worksheet")[3];
+}
 
 function App() {
   const [ckData, setCkData] = useState("");
+  const [tex, setTex] = useState("");
+  const [mml, setMml] = useState("");
+
+  useEffect(() => {
+    async function fetchTex() {
+      const tex = await httpGet("/tex-combined.txt");
+
+      setTex(tex);
+    }
+    async function fetchMML() {
+      const mml = await httpGet("/mml.xml");
+
+      setMml(mml);
+
+      parseXml(mml);
+    }
+
+    fetchTex();
+    fetchMML();
+
+    setTimeout(() => {
+      MathJax.typeset();
+    }, 5000);
+  }, []);
 
   return (
     <div className="App">
@@ -19,12 +59,8 @@ function App() {
         onChange={(event, editor) => {
           const data = editor.getData();
 
+          console.log("data", { data });
           setCkData(data);
-
-          // eslint-disable-next-line no-undef
-          // ref.innerHtml = MathJax.mathml2chtml(data);
-
-          console.log({ data });
         }}
         onBlur={(event, editor) => {
           // eslint-disable-next-line no-undef
@@ -35,11 +71,6 @@ function App() {
         onFocus={(event, editor) => {
           console.log("Focus.", editor);
         }}
-        config={
-          {
-            // toolbar: ["MathType"],
-          }
-        }
       />
       <br />
       <br />
@@ -48,18 +79,27 @@ function App() {
       <br />
       <p>
         This is dynamic content from Editor, blur the editor to render from
-        MathML to HTML
+        MathML to HTML. This content combination of HTML and MML
       </p>
-      <div dangerouslySetInnerHTML={{ __html: ckData }}></div>
-      <p>This is STATIC</p>
-      <p>
-        <math xmlns="http://www.w3.org/1998/Math/MathML">
-          <mroot>
-            <mn>2</mn>
-            <mn>1</mn>
-          </mroot>
-        </math>
-      </p>
+      <div
+        dangerouslySetInnerHTML={{ __html: ckData }}
+        style={{ minHeight: 200 }}
+      ></div>
+
+      <p>This content is TEX to HTML</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: tex,
+        }}
+        style={{ minHeight: 200 }}
+      ></div>
+      <p>This content is MML+XML to HTML</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: mml,
+        }}
+        style={{ minHeight: 200, display: "none" }}
+      ></div>
     </div>
   );
 }
